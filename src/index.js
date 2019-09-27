62e437ed6d4fcf2e8846e1e1fdeb98f1cff9545e
igor.zd.43@gmail.com <igor.zd.43@gmail.com>
2019-09-27 14:34:55 +0300

    change root react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
+import { Provider } from "../node_modules/react-redux";
 import store from "./store";
 import App from './App';
 import './style/App.scss';
