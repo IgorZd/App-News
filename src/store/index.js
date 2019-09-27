35c1cd063eb30cd7fcf888ed95959fde173948ee
igor.zd.43@gmail.com <igor.zd.43@gmail.com>
2019-09-27 14:47:07 +0300

    add root redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from "../../node_modules/redux";
 import rootReducer from "./reducers/index";
 import { rootMiddleware } from "./middleware";
-import thunk from "redux-thunk";
+import thunk from "../../node_modules/redux-thunk";
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
